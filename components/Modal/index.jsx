3449693004a0c26da8f8c374cd46eae29503a42d
Andrew Akeroyd <andrew.akeroyd@simplehq.co>
2016-10-19 12:19:32 +1000

    bugfix: modals have optional onOpen and onClose callbacks, but these were erroring out if not set

--- components/Modal/index.jsx
+++ components/Modal/index.jsx
@@ -20,9 +20,9 @@ class Modal extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.open !== prevState.open) {
       if (this.state.open) {
-        this.props.onOpen();
+        if (this.props.onOpen) { this.props.onOpen(); }
       } else {
-        this.props.onClose();
+        if (this.props.onClose) { this.props.onClose(); }
       }
     }
   }
