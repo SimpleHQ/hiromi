ca9914516d77b6a824482bcf2ad3484389a5c299
Andrew Akeroyd <andrew.akeroyd@simplehq.co>
2016-11-28 15:01:38 +1000

    convert textarea to stageful component, so we can access the value through refs

--- components/Textarea/index.jsx
+++ components/Textarea/index.jsx
@@ -1,16 +1,18 @@
-import React, {PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import {
   modifierClassList, defaultReactProps, defaultReactPropsValues
 } from '../utils';
 
-const Textarea = ({className, ...props}) => {
-  let {classList, ...finalProps} = modifierClassList(props);
-  classList = classnames('textarea', className, classList);
-  return (
-    <textarea className={classList} {...finalProps}></textarea>
-  );
-};
+class Textarea extends Component {
+  render() {
+    let {classList, className, ...finalProps} = modifierClassList(this.props);
+    classList = classnames('textarea', className, classList);
+    return (
+      <textarea className={classList} ref="element" {...finalProps}></textarea>
+    );
+  }
+}
 
 Textarea.propTypes = {
   ...defaultReactProps
